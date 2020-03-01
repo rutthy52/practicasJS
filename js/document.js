79207b63a08f38201725362846cbfb78c0af91dc
ruth <rutthy52.rg@gmail.com>
2020-03-01 22:17:34 +0100

    document

--- js/document.js
+++ js/document.js
@@ -9,3 +9,29 @@ caja.style.padding="20px";
 let cambiaColor = (color) =>{
     caja.style.background = color;
 }
+
+
+/*----------  NOTE = buscar todos las tags iguales  ----------*/
+
+
+let todosDivs = document.getElementsByTagName('div');
+console.log(`html collection: ${todosDivs}`);
+
+
+/*----------  NOTE = create Element sacar los datos de unos elementos y pintarlos en otros  ----------*/
+let section = document.getElementById('mi-section');
+
+for(let item in todosDivs){
+    let contenido = document.createTextNode(todosDivs[item].textContent);
+    if (typeOf(contenido == 'string')){
+        let parrafo = document.createElement("p");
+        section.append(parrafo);
+        parrafo.append(contenido);
+    }
+    
+}
+
+
+
+
+
