732425093d3f5984ebb0e92be634ed42f73e9947
ruth <rutthy52.rg@gmail.com>
2020-03-17 19:15:34 +0100

    loca storage

--- js/ejer-localstorage.js
+++ js/ejer-localstorage.js
@@ -5,9 +5,16 @@ window.addEventListener('load', ()=>{
 
 let formulario = document.getElementById('form')
 
-let addTitle = (dato)=> {
-let title = document.getElementById('titulo').value;
- console.log(dato)
+let addTitle = ()=> {
+
+let title = new Array();
+
+title = document.getElementById('titulo').value;
+
+for (let i in title){
+    console.log(title[i])
+    }
+
 }
 
 
