eb97884b3dfe1fc1a253f07fb877b1ab7ec5c16b
ruth <rutthy52.rg@gmail.com>
2020-03-18 21:36:06 +0100

    loca storage

--- js/ejer-localstorage.js
+++ js/ejer-localstorage.js
@@ -3,20 +3,18 @@
 
 window.addEventListener('load', ()=>{
 
-let formulario = document.getElementById('form')
+let formulario = document.getElementById('add')
 
-let addTitle = (dato)=> {
-let title = document.getElementById('titulo').value;
-let memory = localStorage.setItem('titulo', title)
-document.querySelector('#contenido').innerHTML= localStorage.getItem('titulo')
+let title
 
-}
+formulario.addEventListener('click', () => {
 
+   title = document.getElementById("titulo").value;
+    
+})
 
-
-formulario.addEventListener('submit', addTitle, false)
-
-console.log('.  funciona')
-
+   let datos = [];
+   let arrayDAtos = datos.push(title);
+   console.log(arrayDAtos);
 
 })
