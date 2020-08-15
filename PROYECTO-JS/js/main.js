258cf941924135795127039fac934cb058c5a281
ruth <rutthy52.rg@gmail.com>
2020-08-15 23:37:22 +0200

    jquery y otros

--- PROYECTO-JS/js/main.js
+++ PROYECTO-JS/js/main.js
@@ -1,34 +1,68 @@
 "use strict";
-window.addEventListener('load', ()=>{
-     document.body.style.backgroundImage = 'url("./img/rojo.jpg")'
 
-let setTheme = (bgcolor,bgimage)=>{
+
+window.addEventListener("load", () => {
+
+  let setTheme = (bgcolor) => {
+      function getSourceAsDOM() {
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open(
+          "GET", "http://127.0.0.1:5500/PROYECTO-JS/index.html",
+          false);
+        xmlhttp.send();
+        let parser = new DOMParser();
+        return parser.parseFromString(xmlhttp.responseText, "text/html");
+      }
+
+    let bgImg = document.querySelector("body");
+    bgImg.className=("theme-bg-" + bgcolor);
     let bgHeader = document.querySelector("header h1");
     bgHeader.style.backgroundColor = bgcolor;
-    document.body.style.backgroundImage = "url('" + bgimage + "')";
-    let bgcomments = document.querySelector(".comments");
-    bgcomments.style.backgroundColor = bgcolor;
-   
-    
-}
 
+    if (document.querySelectorAll(".comments") >= 1 ){
+      this.style.backgroundColor = bgcolor;
+    }
+    if (document.querySelectorAll('#carousel')){
+      this.style.backgroundColor = bgcolor;
+    }
+    if (document.querySelectorAll(".ui-state-active")){
+      this.style.backgroundColor = bgcolor;
+    }
+  };
 
-let themeBottom = document.querySelectorAll(".theme-botoms > div ");
-console.log(themeBottom);
+  let themeBottom = document.querySelectorAll(".theme-botoms > div ");
+  console.log(themeBottom);
 
-themeBottom.forEach((elemento) => {
-    elemento.addEventListener('click', ()=>{
-          if(elemento.id === 'red-theme'){
-                console.log(elemento.id);
-                setTheme('red', './img/rojo.jpg')
-          }else if (elemento.id === 'green-theme'){
-                setTheme("green", "./img/hojas.jpg");
-          }else if (elemento.id === 'blue-theme'){
-                 setTheme("blue", "./img/cielo.jpg");
-          }
+  themeBottom.forEach((elemento) => {
+    elemento.addEventListener("click", () => {
+      if (elemento.id === "red-theme") {
+        console.log(elemento.id);
+        setTheme("red");
+      } else if (elemento.id === "green-theme") {
+        setTheme("green");
+      } else if (elemento.id === "blue-theme") {
+        setTheme("blue");
+      }
+    });
+  });
 
-    })
+  /*$(document).ready(function () {
+      $("#date-publication").datepicker(
+      );
+      $("#slider").nivoSlider();
+      $("#accordion").accordion();
+
+  });*/
+
+  let getHora = ()=>{
+
+    let fecha = new Date();
+    let hora = fecha.getHours();
+    let min = fecha.getMinutes();
+    let sec = fecha.getSeconds();
+    document.querySelectorAll(".reloj").innerHTML = `${hora}:${min}:${sec}`;
+  }
+  getHora();
+
 
 });
-
-})
