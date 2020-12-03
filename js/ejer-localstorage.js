32368eedb52623d95b931dc5bd3fdca273f4388e
rutthy52rg <rutthy52.rg@gmail.com>
2020-12-03 21:24:39 +0100

    actualiciones

--- js/ejer-localstorage.js
+++ js/ejer-localstorage.js
@@ -3,20 +3,21 @@
 
 window.addEventListener('load', ()=>{
 
+
 let formulario = document.getElementById('form')
 
-let addTitle = (dato)=> {
-let title = document.getElementById('titulo').value;
-let memory = localStorage.setItem('titulo', title)
-document.querySelector('#contenido').innerHTML= localStorage.getItem('titulo')
+let dato
+let addTitle = ()=> {
 
+dato = document.getElementById('titulo').value;
+console.log(dato);
+return dato
 }
+console.log(dato)
+let arrayDatos = [];
+arrayDatos.push(dato);
+console.log(arrayDatos);
 
-
-
-formulario.addEventListener('submit', addTitle, false)
-
-console.log('.  funciona')
-
-
+if (formulario.addEventListener('submit'))
+    addTitle()
 })
